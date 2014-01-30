8d4d37303c2ac2f1dd68ac4aec258cb57d929110
bejar <bejar@lsi.upc.edu>
2014-01-30 10:23:39 +0100

    Adding Register agent

---
+++ RegisterAgent.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 30/1/2014
+
+Agente que guarda un registro de otros agentes
+
+Guarda los registros como tripletas RDF
+Usad OWL-S profile como ontologia
+
+@author: bejar
+"""
+
+__author__ = 'bejar'
